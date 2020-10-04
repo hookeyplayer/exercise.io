a0a22880855e358cc94c4af0efc8e043e0678a89
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-10-05 01:24:00 +0800

    Update dictionary.py

--- thinkpython2/dictionary.py
+++ thinkpython2/dictionary.py
@@ -43,11 +43,30 @@ def inverse(s):
         else:   
             inverse[val].append(key)
     return inverse
-test = inverse(h)
-print(type(test))
-test
-    
-    
+
+# =============================================================================
+#  official edition
+# =============================================================================
+
+def invert_dict(d):
+    """Inverts a dictionary, returning a map from val to a list of keys.
+    If the mapping key->val appears in d, then in the new dictionary
+    val maps to a list that includes key.
+    d: dict
+    Returns: dict
+    """
+    inverse = {}
+    for key in d:
+        val = d[key]
+        inverse.setdefault(val, []).append(key)
+    return inverse
+
+if __name__ == '__main__':
+    d = dict(a=1, b=2, c=3, z=1)
+    inverse = invert_dict(d)
+    for val in inverse:
+        keys = inverse[val]
+        print(val, keys)    
     
     
     

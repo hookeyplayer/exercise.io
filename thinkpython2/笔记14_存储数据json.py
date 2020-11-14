627bedafc24e87a7c514f35cd63a2f600dfb54c3
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-11-14 23:03:54 +0800

    Add files via upload

---
+++ thinkpython2/笔记14_存储数据json.py
@@ -0,0 +1,32 @@
+import json
+
+# numbers = [2, 3, 5, 7, 11, 13]
+# filename = 'numbers.json'
+# with open(filename, 'w') as f_obj:
+# 	json.dump(numbers, f_obj) #存储到文件number里
+
+# with open(filename) as f_obj:
+# 	numbers = json.load(f_obj) #加载存储在number里的信息
+# 	print(numbers)
+
+username = input("What is your name? ")
+# 存储用户名字
+filename = 'username.json'
+# with open(filename, 'w') as f_obj:
+# 	json.dump(username, f_obj)
+# 	print("We'll remember you when you come back, " + username + "!")
+# # 向名字被储存的用户问候
+# filename = 'username.json'
+# with open(filename) as f_obj:
+# 	username = json.load(f_obj)
+# 	print("Welcome back, " + username + "!")
+try:
+	with open(filename) as f_obj:
+		username = json.load(f_obj)
+	except FileNotFoundError:
+		username = input("What is your name? ")
+		with open(filename, 'w') as f_obj:
+			json.dump(username, f_obj)
+			print("We'll remember you when you come back, " + username + "!")
+	else:
+		print("Welcome back, " + username + "!")

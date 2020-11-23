eb9e9f8d7338e571fe71b733374c31c0b8af4dc1
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-11-23 23:49:14 +0800

    Update 笔记12_文件读取.py

--- thinkpython2/笔记12_文件读取.py
+++ thinkpython2/笔记12_文件读取.py
@@ -1,18 +1,29 @@
-# 如果读取的是数字，就必须使用函数int()将其转换为整数
-# 或使用函数float()将其转换为浮点数
+# read()到达文件末尾时返回一个空字符串，所以删除字符串末尾空白
+with open('pi_digits.txt') as file_object:
+	contents = file_object.read()
+	print(contents.rstrip())
+	
+# 经由路径读取
+with open('text_files/filename.txt') as file_object:
 
-# with open('/Users/xiaofan/python练习/pi_digits.txt') as file_object:
-# 	contents = file_object.read() 
-# 	print(contents.rstrip())
-# 	lines = file_object.readlines()
-#	逐行读取
-#	for line in lines:
-#		print(line.strip())
+# 逐行读取
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+	for line in file_object:
+		print(line.rstrip())
 
-# 打印
+# 创建包含各行的列表
+# readlines()从文件中读取每一行，并将其存储在一个列表中
+# 列表lines的每个元素都对应于文件中的一行
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+	lines = file_object.readlines()
+for line in lines:
+	print(line.rstrip())
+	
+# 三行连起来显示pi
 with open('/Users/xiaofan/python练习/pi_digits.txt') as file_object:
 		lines = file_object.readlines()
-
 pi_string = ''
 for line in lines:
 	# pi_string += line.rstrip()

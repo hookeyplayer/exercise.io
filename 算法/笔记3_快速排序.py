d67f9a2573797125b83f8313e7565137815e2a60
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-11-25 15:04:25 +0800

    Update 笔记3_快速排序.py

--- 算法/笔记3_快速排序.py
+++ 算法/笔记3_快速排序.py
@@ -2,8 +2,11 @@ def quicksort(array):
 	if len(array) <= 1:
 		return array
 	else:
+		# 设置基准元素
 		pivot = array[0]
+		# 分成一小一大左右两组
 		smaller = [i for i in array[1:] if i < pivot]
 		bigger = [i for i in array[1:] if i > pivot]
+		# 分而治之
 		return quicksort(smaller) + [pivot] + quicksort(bigger)
 print(quicksort([8, 3, 5, 1, 2, 9]))

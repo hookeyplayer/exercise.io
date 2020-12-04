12fdc3422caf537f6a9638a3ad9bafb05fdad600
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-04 15:22:50 +0800

    Update 笔记08_二次问题(检查重复值).js

--- 算法/笔记08_二次问题(检查重复值).js
+++ 算法/笔记08_二次问题(检查重复值).js
@@ -1,4 +1,4 @@
-// 法一：O(N^2)
+// 法一：嵌套循环，时间复杂度O(N^2)，空间复杂度O(1)
 function has_duplicate_value(array) {
 
 	// var i 遍历数组元素；当i指向下一元素时发起第2个for循环，用var j遍历；
@@ -18,12 +18,12 @@ function has_duplicate_value(array) {
 	return false;
 }
 
-// 法二：O(N)
+// 法二：时间复杂度O(N)，但占用额外内存,O(N)
 function has_duplicate_value2(array) {
 	var existing_nums = [];
 
 	// 只有1个循环；
-	// 每发现新数字，就以其为索引找出existing_nums中对应的格子并赋值1；
+	// array里每发现新数字，就以其为索引找出existing_nums中对应的格子并赋值1；
 	
 	for(var i = 0; i < array.length; i++) {
 		if(existing_nums[array[i]] === undefined) {

48e243105088b54a4d229188f5f3d4f8c0401ef4
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-10 14:20:55 +0800

    Add files via upload

---
+++ 算法/string_242_valid anagram.py
@@ -0,0 +1,23 @@
+# anagram:变换字母顺序，变成另一个词
+class Solution(object):
+# 1
+    # def isAnagram(self, s, t):
+    #     """
+    #     :type s: str
+    #     :type t: str
+    #     :rtype: bool
+    #     """
+    #     return sorted(s) == sorted(t)
+
+# 2: hashmap
+    def isAnagram(self, s, t):
+    	hms, hmt = {}, {}
+    	for char in s:
+    		hms[char] = hms.get(char, 0) + 1
+    	for char in t:
+    		hmt[char] = hmt.get(char, 0) + 1
+    	return hms == hmt
+# 3
+	def isAnagram(self, s, t):
+		from collections import Counter
+		return Counter(s) == Counter(t)

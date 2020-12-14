e9656afc8941168f25637f26c1f3f9d6037e331c
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-14 14:07:36 +0800

    Add files via upload

---
+++ SQL数据库/笔记09_statistics.sql
@@ -0,0 +1,153 @@
+CREATE TABLE acs_2011_2015_stats (
+    geoid varchar(14) CONSTRAINT geoid_key PRIMARY KEY,
+    county varchar(50) NOT NULL,
+    st varchar(20) NOT NULL,
+    pct_travel_60_min numeric(5,3) NOT NULL,
+    pct_bachelors_higher numeric(5,3) NOT NULL,
+    pct_masters_higher numeric(5,3) NOT NULL,
+    median_hh_income integer,
+    CHECK (pct_masters_higher <= pct_bachelors_higher)
+);
+
+COPY acs_2011_2015_stats
+FROM '/Users/xiaofan/Downloads/practical-sql-master/Chapter_10/acs_2011_2015_stats.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+SELECT * FROM acs_2011_2015_stats;
+
+-- 教育和收入的Pearson相关性
+SELECT corr(median_hh_income, pct_bachelors_higher)
+    AS bachelors_income_r
+FROM acs_2011_2015_stats; --0.6821
+
+-- 其他相关性
+SELECT
+    round(
+      corr(median_hh_income, pct_bachelors_higher)::numeric, 2
+      ) AS bachelors_income_r,
+    round(
+      corr(pct_travel_60_min, median_hh_income)::numeric, 2
+      ) AS income_travel_r,
+    round(
+      corr(pct_travel_60_min, pct_bachelors_higher)::numeric, 2
+      ) AS bachelors_travel_r
+FROM acs_2011_2015_stats;
+
+-- 用least squares回归来预测
+-- slope, intercept
+SELECT
+    round(
+        regr_slope(median_hh_income, pct_bachelors_higher)::numeric, 2
+        ) AS slope,
+    round(
+        regr_intercept(median_hh_income, pct_bachelors_higher)::numeric, 2
+        ) AS y_intercept
+FROM acs_2011_2015_stats;
+
+-- r-squared
+SELECT round(
+        regr_r2(median_hh_income, pct_bachelors_higher)::numeric, 3
+        ) AS r_squared
+FROM acs_2011_2015_stats;
+
+-- Variance
+SELECT var_pop(median_hh_income)
+FROM acs_2011_2015_stats;
+
+-- Standard deviation of the entire population
+SELECT stddev_pop(median_hh_income)
+FROM acs_2011_2015_stats;
+
+-- Covariance
+SELECT covar_pop(median_hh_income, pct_bachelors_higher)
+FROM acs_2011_2015_stats;
+
+-- rank() and dense_rank() 
+-- both window functions:proform calculations across sets of rows using OVER 
+-- rank的rank order含gap, e.g., 1\2\3\3\5, dense_rank连续, e.g.,1/2/3/3/4
+CREATE TABLE widget_companies (
+    id bigserial,
+    company varchar(30) NOT NULL,
+    widget_output integer NOT NULL
+);
+
+INSERT INTO widget_companies (company, widget_output)
+VALUES
+    ('Morse Widgets', 125000),
+    ('Springfield Widget Masters', 143000),
+    ('Best Widgets', 196000),
+    ('Acme Inc.', 133000),
+    ('District Widget Inc.', 201000),
+    ('Clarke Amalgamated', 620000),
+    ('Stavesacre Industries', 244000),
+    ('Bowers Widget Emporium', 201000);
+
+SELECT
+    company,
+    widget_output,
+    rank() OVER (ORDER BY widget_output DESC),
+    dense_rank() OVER (ORDER BY widget_output DESC)
+FROM widget_companies;
+
+-- Applying rank() within sub groups (例如两列都是key) using PARTITION BY
+CREATE TABLE store_sales (
+    store varchar(30),
+    category varchar(30) NOT NULL,
+    unit_sales bigint NOT NULL,
+    CONSTRAINT store_category_key PRIMARY KEY (store, category)
+);
+
+INSERT INTO store_sales (store, category, unit_sales)
+VALUES
+    ('Broders', 'Cereal', 1104),
+    ('Wallace', 'Ice Cream', 1863),
+    ('Broders', 'Ice Cream', 2517),
+    ('Cramers', 'Ice Cream', 2112),
+    ('Broders', 'Beer', 641),
+    ('Cramers', 'Cereal', 1003),
+    ('Cramers', 'Beer', 640),
+    ('Wallace', 'Cereal', 980),
+    ('Wallace', 'Beer', 988);
+
+-- how each store’s sales ranks within each category
+SELECT
+    category,
+    store,
+    unit_sales,
+    rank() OVER (PARTITION BY category ORDER BY unit_sales DESC)
+FROM store_sales;
+
+-- Listing 10-8: Create and fill a 2015 FBI crime data table
+
+CREATE TABLE fbi_crime_data_2015 (
+    st varchar(20),
+    city varchar(50),
+    population integer,
+    violent_crime integer,
+    property_crime integer,
+    burglary integer,
+    larceny_theft integer,
+    motor_vehicle_theft integer,
+    CONSTRAINT st_city_key PRIMARY KEY (st, city)
+);
+
+COPY fbi_crime_data_2015
+FROM '/Users/xiaofan/Downloads/practical-sql-master/Chapter_10/fbi_crime_data_2015.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+SELECT * FROM fbi_crime_data_2015
+ORDER BY population DESC;
+
+-- Listing 10-9: Find property theft crime rates per thousand residents in cities with 500,000
+-- or more people
+SELECT
+    city,
+    st,
+    population,
+    property_crime,
+    round(
+        (property_crime::numeric / population) * 1000, 1
+        ) AS pc_per_1000 
+FROM fbi_crime_data_2015
+WHERE population >= 500000
+ORDER BY (property_crime::numeric / population) DESC;

297ff2c300184c1282ce2d45f184f7da499df976
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-22 01:23:45 +0800

    Update string_242_valid anagram.py

--- 算法/string_242_valid anagram.py
+++ 算法/string_242_valid anagram.py
@@ -1,22 +1,21 @@
 # anagram:变换字母顺序，变成另一个词
 class Solution(object):
 # 1
-    # def isAnagram(self, s, t):
-    #     """
-    #     :type s: str
-    #     :type t: str
-    #     :rtype: bool
-    #     """
-    #     return sorted(s) == sorted(t)
-
-# 2: hashmap
-    def isAnagram(self, s, t):
+	def isAnagram(self, s, t):
     	hms, hmt = {}, {}
     	for char in s:
     		hms[char] = hms.get(char, 0) + 1
     	for char in t:
     		hmt[char] = hmt.get(char, 0) + 1
     	return hms == hmt
+# 2
+	def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
 # 3
 	def isAnagram(self, s, t):
 		from collections import Counter

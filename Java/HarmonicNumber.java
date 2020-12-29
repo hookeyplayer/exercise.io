6d65a505c974ff5feae6f4145576e6aca141bd22
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-29 21:19:08 +0300

    Add files via upload

--- Java/HarmonicNumber.java
+++ Java/HarmonicNumber.java
@@ -1,13 +1,28 @@
+// public class HarmonicNumber
+// {
+// 	public static void main(String[] args)
+// 	{
+// 		int n = Integer.parseInt(args[0]);
+// 		double sum = 0.0;
+// 		for (int i = 1; i <= n; i++) sum += 1.0 / i;
+// 		System.out.println(sum);
+// 	}
+// }
 public class HarmonicNumber
 {
+	public static double harmonic(int n)
+	{
+		double sum = 0.0;
+		for (int i = 1; i <= n; i++) sum += 1.0 / i;
+		return sum;
+	}
 	public static void main(String[] args)
 	{
-		int n = Integer.parseInt(args[0]);
-		double sum = 0.0;
-		for (int i = 1; i <= n; i++)
+		for (int i = 0; i < args.length; i++)
 		{
-			sum += 1.0/i;
+			int arg = Integer.parseInt(args[i]);
+			double value = harmonic(arg);
+			System.out.println(value);
 		}
-		System.out.println(sum);
 	}
 }
---
+++ Java/countOnes.java
@@ -0,0 +1,13 @@
+// O(logn)
+// C++
+int countones(unsigned int n)
+{
+	int ones = 0; //计数器复位
+	while (0 < n)
+	{
+		ones += (1 & n); //二进制位的与运算，检查最低位
+		n >> 1; // 右移1位，n减半
+	}
+	return ones;
+}
+// glibc 内置函数 int __builtin_popcount (unsigned int n)

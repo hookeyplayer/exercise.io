cc52ff12c2da22c3cb0f5b4c0754eced614a3bbf
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-29 11:33:09 +0300

    编码最小距离，最小二进制位数的差异

--- 算法/461_hamming distance_不会.py
+++ 算法/461_hamming distance_不会.py
@@ -1,5 +1,3 @@
-# The Hamming distance between two integers is:
-# the number of positions at which the corresponding bits are different.
 class Solution(object):
     def hammingDistance(self, x, y):
         """

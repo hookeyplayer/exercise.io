170307d05590e5990ba25927e23f81e58108085e
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-30 08:11:21 +0300

    独特花色抽牌

---
+++ Java/CouponCollector.java
@@ -0,0 +1,45 @@
+// public class CouponCollector {
+// 	public static void main(String[] args) {
+// 		int n = Integer.parseInt(args[0]);
+// 		boolean[] isCollected = new boolean[n];
+// 		int count = 0;
+// 		int distinct = 0; 
+// 		while (distinct < n) {
+// 			int r = (int) (Math.random()*n);
+// 			count ++;
+// 			if (!isCollected[r]) {
+// 				distinct++;
+// 				isCollected[r] = true;
+// 			}
+// 		}
+// 	}
+// 	System.out.println(count);
+// }
+
+public class CouponCollector {
+	//抽取
+	public static int getCoupon(int n) {
+		return (int) (Math.random()*n);
+	}
+
+	public static int collectCoupons(int n) {
+		boolean[] isCollected = new boolean[n];
+		int count = 0;
+		int distinct = 0; 
+		while (distinct < n) {
+			int r = getCoupon(n);
+			count ++;
+			if (!isCollected[r]) {
+				distinct++;
+				isCollected[r] = true;
+			}	
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int count = collectCoupons(n);
+		System.out.println(count);
+	}
+	
+}

dc9b994f090f819147da7e69122141bb9a010339
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2020-12-31 15:29:12 +0300

    recursion: Wait for Godots

---
+++ Java/Beckett.java
@@ -0,0 +1,13 @@
+public class Beckett {
+	public static void moves(int n, boolean enter) {
+		if (n == 0) return;
+		moves(n-1, true);
+		if (enter) System.out.println("enter " + n);
+		else System.out.println("exit " + n);
+		moves(n-1, false);
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		moves(n, true);
+	}
+}

1e9dd8497c2e165924ccb1533f0dd5f63404c0d2
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-03 09:57:54 +0300

    递归查表+DP

--- 算法/笔记00_硬币找零.py
+++ 算法/笔记00_硬币找零.py
@@ -30,7 +30,7 @@ print(recDC(coinValueList, change, knownResults))
 # def dpMakeChange(coinValueList, change, minCoins):
 # 	for cents in range(change+1):
 # 		coinCount = cents
-
+ 
 # 		for j in [c for c in coinValueList if c <= cents]:
 # 			if minCoins[cents-j] + 1 < coinCount:
 # 				coinCount = minCoins[cents-j] + 1

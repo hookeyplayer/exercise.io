25804f52e2d657a134c867bf05b074287432bb84
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-03 11:06:43 +0300

    快速排序，归并

--- 算法/笔记03_快速排序_D&C.py
+++ 算法/笔记03_快速排序_D&C.py
@@ -5,5 +5,5 @@ def quicksort(array):
 		pivot = array[0] # 设置基准元素
 		smaller = [i for i in array[1:] if i < pivot]
 		bigger = [i for i in array[1:] if i > pivot] # 分成一小一大左右两组
-		return quicksort(smaller) + [pivot] + quicksort(bigger) # 分而治之
+		return quicksort(smaller) + [pivot] + quicksort(bigger)
 print(quicksort([8, 3, 5, 1, 2, 9]))

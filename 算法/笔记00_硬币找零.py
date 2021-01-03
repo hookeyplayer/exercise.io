accd4720ea57ff17943b081da744fb15e1c24ab1
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-03 09:57:33 +0300

    递归差别+DP

--- 算法/笔记00_硬币找零.py
+++ 算法/笔记00_硬币找零.py
@@ -27,3 +27,55 @@ print(recDC(coinValueList, change, knownResults))
 
 # 2 dp：从为1美分找零的最优解开始，逐步递加
 # 保证在算法的每一步过程中已知兑换更小数值的最优解
+# def dpMakeChange(coinValueList, change, minCoins):
+# 	for cents in range(change+1):
+# 		coinCount = cents
+
+# 		for j in [c for c in coinValueList if c <= cents]:
+# 			if minCoins[cents-j] + 1 < coinCount:
+# 				coinCount = minCoins[cents-j] + 1
+# 		minCoins[cents] = coinCount
+# 	return minCoins[change]
+
+def dpMakeChange(coinValueList, change, minCoins, coinsUsed):
+	for cents in range(change+1):
+		coinCount = cents 
+		newCoin = 1
+		for j in [c for c in coinValueList if c <= cents]:
+			if minCoins[cents-j] + 1 < coinCount:
+				coinCount = minCoins[cents-j] + 1
+				newCoin = j
+		minCoins[cents] = coinCount
+		coinsUsed[cents] = newCoin
+	return minCoins[change]
+
+def printCoins(coinsUsed, change):
+	coin = change
+	while coin > 0:
+		thisCoin = coinsUsed[coin]
+		print(thisCoin)
+		coin = coin - thisCoin
+
+def main():
+	amount = 63
+	coinlist = [1, 5, 10, 21, 25]
+	# coinsUsed：用来找零的硬币的阵列
+	coinsUsed = [0] * (amount+1)
+	# coinCount：为表中相应位置的量找零所需的硬币数量的最小值
+	coinCount = [0] * (amount+1)
+	print('Make change for ', amount, 'requires:')
+	print(dpMakeChange(coinlist, amount, coinCount, coinsUsed), ' coins')
+	print('They are: ')
+	printCoins(coinsUsed, amount)
+	print('The used list is: ')
+	print(coinsUsed)
+
+main()
+Make change for  63 requires:
+3  coins
+They are: 
+21
+21
+21
+The used list is: 
+[1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 10, 1, 1, 1, 1, 5, 1, 1, 1, 1, 10, 21, 1, 1, 1, 25, 1, 1, 1, 1, 5, 10, 1, 1, 1, 10, 1, 1, 1, 1, 5, 10, 21, 1, 1, 10, 21, 1, 1, 1, 25, 1, 10, 1, 1, 5, 10, 1, 1, 1, 10, 1, 10, 21]

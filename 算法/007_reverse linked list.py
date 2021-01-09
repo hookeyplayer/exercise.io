75986f8c126aad2e093d7a23915a6b14c9b2801c
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-10 03:50:42 +0800

    链表, recursive

---
+++ 算法/007_reverse linked list.py
@@ -0,0 +1,11 @@
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+class Solution(object):
+	def reverseList(self, head):
+		if head is None or head.next is None: return head
+		p = self.reverseList(head.next)
+		head.next.next = head
+		head.next = None
+		return p

7948627ef03be56dc3375902b50cea39244eef92
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-11 11:59:12 +0300

    Update 笔记10_插入排序.py

--- 算法/笔记10_插入排序.py
+++ 算法/笔记10_插入排序.py
@@ -1,31 +1,20 @@
-# insertA[i] into sorted array A[0:i-1]
-# 通过pariwise swaps down to correct position of A[i]
+# insert A[i] into sorted array A[0:i-1]
+# pariwise swaps down to correct position of A[i]
 # 移除：n-1次
 # 比较：1+2+3+...+(n-1)=n(n-1)/2,约为N^2/2
 # 平移：=比较的次数
 # 插入：n-1次
 # 共计(N^2 + 2N -2), 记作O(N^2)
-
 def insertion_sort(array):
-
-	# 从索引1开始的遍历
 	# 变量index保存当前索引
-
-	for index in range(1, len(array)):
-		position = index
-		temp = array[index]
-
-		# 内部循环，检查position左侧的值是否大于temp
-		# 若左侧大于temp，则将其右移一格
-		# 继续往左检查
-
-		while position > 0 and array[position - 1] > temp:
-			array[position] = array[position - 1]
-			position = position - 1
-
+	for i in range(1, len(array)):
+		temp = array[i]
+		while i > 0 and array[i - 1] > temp:
+			array[i] = array[i - 1]
+			i = i - 1
 		# 最后将temp放回空隙
-
-		array[position] = temp
-
+		array[i] = temp
+	return array
+	
 array = [4, 2, 7, 1, 3]
 print(insertion_sort(array))

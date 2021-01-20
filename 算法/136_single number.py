5bb026b0f9ef8770400b878c4b3540eab58a2de0
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-20 20:54:09 +0300

    list, hash, 异或

--- 算法/136_single number.py
+++ 算法/136_single number.py
@@ -1,32 +1,5 @@
-class Solution(object):
-# hash
-	# def singleNumbers(self, nums):
-	# 	dict = {}
-	# 	for i in nums:
-	# 		try:
-	# 			dict.pop(i)
-	# 		except:
-	# 			dict[i] = 1
-	# 	return dict.popitem()[0]
-
-# array
-	# def singleNumbers(self, nums):
-	# 	a = []
-	# 	for i in nums:
-	# 		if i in a:
-	# 			a.remove(i)
-	# 		else:
-	# 			a.append(i)
-	# 	return a.pop()
-
-# 外部包
-	# def singleNumber(self, nums):
-	# 	from collections import Counter
-	# 	cnt = Counter(nums)
-	# 	for k, v in cnt.items():
-	# 		if v == 1:
-	# 			return k
-
+from typing import List
+class Solution:
 # 异或：两个相同的数异或为0；0和任意数异或都为那个数；异或满足交换律
 	def singleNumbers(self, nums):
 		result = 0
@@ -34,8 +7,53 @@ class Solution(object):
 			result ^= nums[i]
 		return result
 
+# 1
+# O(N^2)
+    def singleNumber(self, nums: List[int]) -> int:
+    	for i in nums:
+    		if nums.count(i) == 1:
+    			return i
 
+# 外部包，类似于1的方法
+	def singleNumber(self, nums):
+		from collections import Counter
+		cnt = Counter(nums)
+		for k, v in cnt.items():
+			if v == 1:
+				return k
 
-test = Solution()
-nums = [2, 2, 5, 3, 3]
-print(test.singleNumbers(nums))
+# 2. hash O(1)
+    def singleNumber(self, nums: List[int]) -> int:
+    	dict = {}
+    	for num in nums:
+    		if num in dict:
+    			dict[num] += 1
+    		else:
+    			dict[num] = 1
+    	for key in dict.keys():
+    		if dict[key] == 1:
+    			return key
+
+# 3. hash O(1)
+    def singleNumber(self, nums: List[int]) -> int:
+    	dict = {}
+    	for i in nums:
+    		try:
+    			dict.pop(i)
+    		except:
+    			dict[i] = 1
+    	return dict.popitem()[0]
+
+# 4. array
+	def singleNumbers(self, nums):
+		a = []
+		for num in nums:
+			if num in a:
+				a.remove(num) # 只移走一个
+			else:
+				a.append(num)
+		return a.pop()
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.singleNumber([2, 2, 5, 3, 3]))

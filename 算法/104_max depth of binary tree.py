048b987dccacb4166fe964c1c22868b0f6f2c178
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-20 07:12:27 +0300

    树

--- 算法/104_max depth of binary tree.py
+++ 算法/104_max depth of binary tree.py
@@ -1,17 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+        # 根节点是否为空
         if root is None:
         	return 0
+        # 根节点不为空，则深度 >= 1
+        # 递归
         ld = self.maxDepth(root.left)
         rd = self.maxDepth(root.right)
         return 1 + max(ld, rd)

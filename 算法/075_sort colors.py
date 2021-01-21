5f443db365b38eba11edf110b08e29167419d63a
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-21 22:20:57 +0800

    List, countSort, 三向切分

--- 算法/075_sort colors.py
+++ 算法/075_sort colors.py
@@ -2,7 +2,7 @@
  # E.W.Dijlstra 荷兰国旗问题（三向切分）
 from typing import List
 class Solution:
-# 双指针,快排+三向切分
+# # 双指针,快排+三向切分
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -13,15 +13,30 @@ class Solution:
         while i <= two:
             if nums[i] == 0: # i遇到0就换到左边去
                 nums[zero] = nums[i]
+                # nums[zero], nums[i] = nums[i], nums[zero]
                 i += 1
                 zero += 1
             elif nums[i] == 1: # 遇到1跳过
                 i += 1
             elif nums[i] == 2: # i遇到2就换到右边去
                 nums[two] = nums[i] # two记录第一个非2的位置
+                nums[two], nums[i] = nums[i], nums[two]
                 two -= 1
 
-# count
+# countSort
+    def sortColors(self, nums: List[int]) -> None:
+        count = [0] * 3
+        for num in nums:
+            count[num] += 1
+        pos = 0
+        for i in range(3):
+            while count[i] > 0:
+                nums[pos] = i
+                pos += 1
+                count[i] -= 1
+        return
+
+# counter
     def sortColors(self, nums: List[int]) -> None:
     	from collection import Counter
     	count = Counter(nums)

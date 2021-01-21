9a32ac033a15b187923c792115bfaae1cc177f43
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-21 11:13:33 +0300

    Update 笔记01_选择排序.py

--- 算法/笔记01_选择排序.py
+++ 算法/笔记01_选择排序.py
@@ -1,4 +1,15 @@
-# 给列表排序
+# 不受输入数据的影响，任何情况的时间复杂度不变
+# 每轮遍历都选出最小元素
+# 1
+def selectionSort(arr):
+	for i in range(len(arr)-1): # 遍历n-1次
+		minIndex = i
+		for j in range(i+1, len(arr)):
+			if arr[j] < arr[minIndex]:
+				minIndex = j
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	return arr
+# 2
 def selectionSort(arr):
 	newArr = []
 	for i in range(len(arr)):
@@ -6,14 +17,13 @@ def selectionSort(arr):
 		newArr.append(arr.pop(a))
 	return newArr
 
-print(selectionSort([3, 5, 8, 1, 9, 0]))
-
-# 查找列表里最小值和它的索引键
 def findMinNumber(arr):
-	a = arr[0]
-	a_index = 0
+	pivot = arr[0]
+	minIndex = 0
 	for i in range(1, len(arr)):
-		if arr[i] < a:
-			a = arr[i]
-			a_index = i
-	return a_index
+		if arr[i] < pivot:
+			pivot = arr[i]
+			minIndex = i
+	return minIndex
+
+print(selectionSort([3, 5, 8, 1, 9, 0]))

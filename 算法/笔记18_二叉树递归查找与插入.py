6bbf68fe5966bd6f25d5d8be9e3e0cbe4e3aefb7
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-22 13:54:54 +0300

    递归

--- 算法/笔记18_二叉树递归查找与插入.py
+++ 算法/笔记18_二叉树递归查找与插入.py
@@ -1,12 +1,8 @@
-# 小于父结点的子结点用左箭头来表示，大于父结点的子结点则用右箭头来表示
 def search(value, node):
-	# 如果node不存在
 	if node is None or node.value == value:
 		return node
-	# 如果value小于当前结点node.value
 	elif value < node.value:
 		return search(value, node.leftChild)
-	# 如果value大于当前结点
 	else:
 		return search(value, node.rightChild)
 		

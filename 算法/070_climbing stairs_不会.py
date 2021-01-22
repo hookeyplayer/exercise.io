b0ed3201981ed9d413bd3eb44290f1f0f03a1a2a
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-22 09:23:35 +0300

    DP

--- 算法/070_climbing stairs_不会.py
+++ 算法/070_climbing stairs_不会.py
@@ -1,5 +1,7 @@
+# DP的核心：最优子结构、边界、状态转移公式
 class Solution(object):
 # O(n+2n) = O(n)
+# 备忘录+DP
 	def climbStairs(self, n: int) -> int:
 		if n <= 2:
 			return n
@@ -9,3 +11,23 @@ class Solution(object):
 		for i in range(3, n+1):
 			dp[i] = dp[i-1] + dp[i-2]
 		return dp[n]
+
+# basic DP
+	def climbStairs(self, n: int) -> int:
+		if n <= 2:
+			return n
+		if n == 1:
+			return 1
+		if n == 2:
+			return 2
+		a, b = 1, 2
+		temp = 0
+		for i in range(3, n+1):
+			temp = a + b
+			a = b
+			b = temp
+		return temp
+
+
+test = Solution()
+print(test.climbStairs(6))

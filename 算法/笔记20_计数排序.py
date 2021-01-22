fff23121c449f4788ddfdc59bf8c6b42afcba8e1
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-22 20:56:54 +0300

    Update 笔记20_计数排序.py

--- 算法/笔记20_计数排序.py
+++ 算法/笔记20_计数排序.py
@@ -1,8 +1,10 @@
+# 小范围内，性能快过O(NlogN)
 def countingSort(arr):
-    # 输入的数据值转化为键，储存在开辟的数组空间里
-	bucket = [0] * (max(arr) + 1) # list of zero's
+    # 输入的数据值转化为键，储存在开辟的统计数组空间里
+	bucket = [0] * (max(arr) + 1) # 根据最大值确定统计数组的长度
 	for a in arr:
 		bucket[a] += 1
+	# 遍历统计数组
 	i = 0
 	for j in range(len(bucket)):
 		while bucket[j] > 0: # 对array里出现的数遍历

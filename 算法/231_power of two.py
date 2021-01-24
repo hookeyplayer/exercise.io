0b751f44f041f808f08d44fd44d214e61cb72ca8
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-24 17:51:07 +0300

    二进制，与

--- 算法/231_power of two.py
+++ 算法/231_power of two.py
@@ -1,9 +1,17 @@
+class Solution:
+# 1
+# 计算1的个数
+    def isPowerOfTwo(self, n: int) -> bool:
+    	if n < 0:
+    		return False
+    	binNum = bin(n)
+    	return sum(map(lambda x:int(x), list(binNum[2:]))) == 1
+
+# 2
 # （2的整数幂）-1，二进制全部变成1，且位数降一位
 # （2的整数幂）和（2的整数幂）-1 进行按位与运算结果是：0
-class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
     	return n & (n-1) == 0
     	
 test = Solution()
 print(test.isPowerOfTwo(3))
-        

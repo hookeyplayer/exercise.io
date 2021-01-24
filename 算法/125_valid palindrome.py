429fb7fea101d4b5d2b8a27d0f27f812623a8cfc
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-24 20:28:10 +0300

    回文句判定

--- 算法/125_valid palindrome.py
+++ 算法/125_valid palindrome.py
@@ -1,11 +1,13 @@
 # 只考虑字母和数字
 class Solution:
+# 1
 # filter函数的两个参数：函数、序列；返回boolean为True的相应元素放到一个列表里
 # isalnum方法检测字符串是否由字母和数字组成
     def isPalindrome(self, s: str) -> bool:
     	s = list(filter(str.isalnum, s.lower()))
     	return True if s == s[::-1] else False
-
+    
+# 2
     def isPalindrome(self, s: str) -> bool:
     	alnumsS = [t.lower() for t in s if t.isalnum()]
     	length = len(alnumsS)
@@ -16,8 +18,6 @@ class Solution:
     		if alnumsS[i] != alnumsS[length-i-1]:
     			return False
     	return True
-
-
 
 if __name__ == "__main__":
     test = Solution()

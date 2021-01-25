bcf712f2e81acc575812e7c920e863365867796b
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-25 11:23:09 +0300

    deque 不明白

--- 算法/笔记04_广度优先搜索.py
+++ 算法/笔记04_广度优先搜索.py
@@ -1,9 +1,6 @@
-# 广度优先搜索将找出最短路径
 # hash table（散列表）：包含额外逻辑的数据结构，即字典
-# 防止重复
-# 模拟映射关系：DNS resolution
-# 缓存：页面URL映射到页面数据
-from collections import deque # 创建双端队列,list-like sequence
+
+from collections import deque # 创建双端队列
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
@@ -19,13 +16,14 @@ graph["jonny"] = []
 def person_is_seller(name):
     return name[-1] == 'm'
 
-#%%
 def search(name):
     searched = [] #记录哪些人已经测试过了
     search_que = deque()
     search_que += graph[name]
+    
     while search_que:
         person = search_que.popleft()
+        
         if person_is_seller(person):
             print(person + 'is mango dealer.')
             return True
@@ -33,7 +31,5 @@ def search(name):
             searched.append(person)
             search_que += graph[person]
     return False
-#%%
+
 print(search('you'))
-    
-    

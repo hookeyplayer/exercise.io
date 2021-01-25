9da5dd2481cdeffaf71f1c81afe92559a978f3f0
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-25 09:33:28 +0300

    Update 笔记01_字典.py

--- thinkpython2/笔记01_字典.py
+++ thinkpython2/笔记01_字典.py
@@ -1,3 +1,29 @@
+import random
+dic = {k:random.randint(4, 9) for k in ['a', 'b', 'c']}
+print(dic) # {'a': 9, 'b': 6, 'c': 6}
+
+
+根据键排序
+# 1. zip
+dic = {'f': 'man', 'b': 'woman', 'c': 'animal'}
+foo = zip(dic.keys(), dic.values())
+foo = [i for i in foo]
+print(foo) # [('a', 'man'), ('b', 'woman'), ('c', 'animal')]
+a = sorted(foo, key=lambda x:x[0])
+print(a) # [('b', 'woman'), ('c', 'animal'), ('f', 'man')]
+# 排序完构造新字典
+new_dic = {i[0]:i[1] for i in a}
+print(new_dic) # {'b': 'woman', 'c': 'animal', 'f': 'man'}
+
+# 2
+dic = {'f': 'man', 'b': 'woman', 'c': 'animal'}
+print(dic.items()) # dict_items([('f', 'man'), ('b', 'woman'), ('c', 'animal')])
+b = sorted(dic.items(), key=lambda x:x[0])
+print(b) # [('b', 'woman'), ('c', 'animal'), ('f', 'man')]
+new_dic = {i[0]:i[1] for i in b}
+print(new_dic)
+
+
 # 根据key从小到大排序
 dic = {"name":"zs",
 "age":18,

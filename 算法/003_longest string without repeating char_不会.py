b4aace9e5d9687aaabc84d2236617c0c1ea0dd12
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-26 08:36:16 +0300

    滑动窗口

--- 算法/003_longest string without repeating char_不会.py
+++ 算法/003_longest string without repeating char_不会.py
@@ -1,94 +1,92 @@
-# # given "abcabc", 给出子串的长度3
+# 无重复最长子串
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        exist = [False] * 256
-        length = len(s)
-        max_len = i = 0
-        for j in range(length):
-            while(exist[ord(s[j])]):
-                exist[ord(s[i])] = False
-                i += 1
-            exist[ord(s[j])] = True
-            max_len = max(max_len, j - i + 1)
+    # def lengthOfLongestSubstring(self, s):
+    #     exist = [False] * 256
+    #     length = len(s)
+    #     max_len = i = 0
 
-        return max_len
+    #     for j in range(length):
 
-    def lengthOfLongestSubstring(self, s):
-        left, right = 0, 0
-        res = 0
-        char_map = {}
-        for right in range(len(s)):
-            if s[right] in char_map:
-                left = max(left, char_map[s[right]] + 1)
-            char_map[s[right]] = right
-            res = max[res, right - left + 1]
-        return res
+    #         while (exist[ord(s[j])]):
+    #             exist[ord(s[i])] = False
+    #             i += 1
 
-    def lengthOfLongestSubstring(self, s):
-        char_map = {}
-        for i in range(256):
-            char_map[i] = -1
-        length = len(s)
-        i = max_len = 0
-        # exists duplicate in current i, j
-        for j in range(length): 
-            if char_map[ord(s[j])] >= i:
-                i = char_map[ord(s[j])] + 1
-            char_map[ord(s[j])] = j
-            max_len = max(max_len, j - i + 1)
-        return max_len
+    #         exist[ord(s[j])] = True
+    #         max_len = max(max_len, j - i + 1)
+
+    #     return max_len
+
+    # def lengthOfLongestSubstring(self, s):
+    #     char_map = {}
+    #     for i in range(256):
+    #         char_map[i] = -1
+    #     length = len(s)
+    #     i = max_len = 0
+    #     # exists duplicate in current i, j
+    #     for j in range(length): 
+    #         if char_map[ord(s[j])] >= i:
+    #             i = char_map[ord(s[j])] + 1
+    #         char_map[ord(s[j])] = j
+    #         max_len = max(max_len, j - i + 1)
+    #     return max_len
         
 # 1:hash+双指针法+sliding window
 # O(N)
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLenthAns = []
-        tempAns = []
-        if len(s) == 1:
-            return len(s)
-        for index, char in enumerate(s):
-            if char not in tempAns:
-                tempAns.append(char)
-            else:
-                del_index = tempAns.index(char)
-                del tempAns[:del_index+1]
-                tempAns.append(char)
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     maxLenthAns = []
+    #     tempAns = []
+    #     if len(s) == 1:
+    #         return len(s)
+    #     for index, char in enumerate(s):
 
-            if len(maxLenthAns) < len(tempAns):
-                maxLenthAns = tempAns[:]
-        return len(maxLenthAns)
+    #         if char not in tempAns:
+    #             tempAns.append(char)
+    #         else:
+    #             del_index = tempAns.index(char)
+    #             del tempAns[:del_index+1]
+    #             tempAns.append(char)
 
+    #         if len(maxLenthAns) < len(tempAns):
+    #             maxLenthAns = tempAns[:]
+
+    #     return len(maxLenthAns)
 
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right = 0, 0
-        char_map = {}
+        char_map = []
         res = 0
         while left < len(s) and right < len(s):
             if s[right] in char_map:
+
                 if s[left] in char_map:
                     char_map.remove(s[left])
                 left += 1
-            else:
-                char_map.add(s[right])
+
+            else: # 右移指针
+                char_map.append(s[right])
                 right += 1
-                res = max(rex, len(char))
+                res = max(res, len(char_map))
         return res
 
 # queue
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        ans = []
-        max = 0
-        for element in s:
-            if len(ans) == 0 or element not in ans:
-                ans.append(element)
-                cur = len(ans)
-                if cur > max:
-                    max = cur
-            else:
-                while ans[0] != element:
-                    ans.remove(ans[0])
-                ans.remove(element)
-                ans.append(element)
-        return max
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     ans = []
+    #     max = 0
+    #     for element in s:
+    #         if len(ans) == 0 or element not in ans:
+
+    #             ans.append(element)
+    #             cur = len(ans)
+    #             if cur > max:
+    #                 max = cur
+
+    #         else:
+    #             while ans[0] != element:
+    #                 ans.remove(ans[0])
+    #             ans.remove(element)
+    #             ans.append(element)
+                
+    #     return max
 
 test = Solution()
 print(test.lengthOfLongestSubstring('abcabc'))

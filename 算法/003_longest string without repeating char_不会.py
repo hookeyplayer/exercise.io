9575c2dab4f2a8097fdabfb1c497cfacb48d5ad0
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-26 09:35:09 +0300

    双边队列，滑动窗口

--- 算法/003_longest string without repeating char_不会.py
+++ 算法/003_longest string without repeating char_不会.py
@@ -32,25 +32,26 @@ class Solution(object):
         
 # 1:hash+双指针法+sliding window
 # O(N)
-    # def lengthOfLongestSubstring(self, s: str) -> int:
-    #     maxLenthAns = []
-    #     tempAns = []
-    #     if len(s) == 1:
-    #         return len(s)
-    #     for index, char in enumerate(s):
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLenthAns = []
+        tempAns = []
+        if len(s) == 1:
+            return len(s)
+        for index, char in enumerate(s):
 
-    #         if char not in tempAns:
-    #             tempAns.append(char)
-    #         else:
-    #             del_index = tempAns.index(char)
-    #             del tempAns[:del_index+1]
-    #             tempAns.append(char)
+            if char not in tempAns:
+                tempAns.append(char)
+            else:
+                del_index = tempAns.index(char)
+                del tempAns[:del_index+1]
+                tempAns.append(char)
 
-    #         if len(maxLenthAns) < len(tempAns):
-    #             maxLenthAns = tempAns[:]
+            if len(maxLenthAns) < len(tempAns):
+                maxLenthAns = tempAns[:]
 
-    #     return len(maxLenthAns)
+        return len(maxLenthAns)
 
+    
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right = 0, 0
         char_map = []
@@ -68,10 +69,33 @@ class Solution(object):
                 res = max(res, len(char_map))
         return res
 
+# 双边队列
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        from collections import deque
+        d_queue = deque([])
+        # 队列中的所有元素
+        visited = set() # set不是字典
+        max_count = 0
+
+        for char in s:
+            # visited 中的元素和 deque 中的元素只有位置不同
+            # 如果需要新增在队尾的元素 char 在 queue 中已经存在
+            if char in visited:
+                # 从队列首部开始移除元素，直到遇到重复的元素为止
+                while d_queue and char in visited:
+                    visited.remove(d_queue.popleft())
+
+            visited.add(char)
+            d_queue.append(char)
+            # 更新最长子串的长度
+            max_count = max(len(d_queue), max_count)
+        return max_count
+
 # queue
     # def lengthOfLongestSubstring(self, s: str) -> int:
     #     ans = []
     #     max = 0
+        
     #     for element in s:
     #         if len(ans) == 0 or element not in ans:
 

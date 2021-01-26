2919bca6ff68e1df80cd1943f5cab6c232f2c3de
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-26 09:25:31 +0300

    链表，不会

--- 算法/002_add two numbers.py
+++ 算法/002_add two numbers.py
@@ -4,27 +4,45 @@ class ListNode(object):
         self.next = next
         
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        carry = 0 # 进位的值
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        root = ListNode(0)
+        temp = ListNode(0)
+        while l1 or l2 or carry:
+            v1, v2 = 0, 0
+
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+
+            # 和10做除法：1, 5 = divmod(15, 10)
+            carry, value = divmod(v1 + v2 + carry, 10)
+            temp.next = ListNode(value)
+            temp = temp.next
+
+        return root.next
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0 # 两数相加需要进位的情况
         head = cur = ListNode(0)
         while l1 or l2:
-        	val = carry
-        	if l1:
-        		val += l1.val
-        		l1 = l1.next
-        	if l2:
-        		val += l2.val
-        		l2 = l2.next
-        	cur.next = ListNode(val % 10)
-        	cur = cur.next
-        	carry = val / 10
+            val = carry
+
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+
+            cur.next = ListNode(val % 10)
+            cur = cur.next
+            carry = val / 10
         if carry > 0:
-        	curr.next = ListNode(carry)
+            curr.next = ListNode(carry)
         return head.next
     
 # Input: l1 = [2,4,3], l2 = [5,6,4]

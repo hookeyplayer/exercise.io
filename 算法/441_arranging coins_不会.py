9824e6cedc69ee779ef05d83d866fd5b269bc1b2
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-26 23:19:43 +0300

    二分

--- 算法/441_arranging coins_不会.py
+++ 算法/441_arranging coins_不会.py
@@ -1,8 +1,16 @@
-# 给定n枚硬币排阶梯，k行有k个，返回k值
-
+# 给定n枚硬币排阶梯，k行有k个，返回full staircase rows的数量
 # 法一：二分查找
 class Solution(object):
-    def arrange_coins(self, n):
+# 超时
+    # def arrange_coins(self, n: int) -> int:
+    # 	i = 1
+    # 	while n-i >= 0:
+    # 		n -= i
+    # 		i += 1
+    # 	return i-1
+
+# 求和公式
+    def arrange_coins(self, n: int) -> int:
         
         # 1 +2 + 3 + ... + k = (k + 1) * k / 2 < n
         
@@ -23,8 +31,13 @@ class Solution(object):
 # 法二：求根公式
 class Solution(object):
     def arrange_coins(self, n):
-
-        # 1 +2 + 3 + ... + k = (k + 1) * k / 2 < n
         
         import math
         return (-1 + int(math.sqrt(1 + 8 * n))) // 2
+
+
+test = Solution()
+n = 5
+print(test.arrange_coins(n)) # 2
+n = 8
+print(test.arrange_coins(n)) # 3

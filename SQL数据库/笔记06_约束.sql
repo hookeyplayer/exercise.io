f6b4f17355399544422f359a7d948b79e3bf74e9
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-29 15:02:08 +0300

    Update 笔记06_约束.sql

--- SQL数据库/笔记06_约束.sql
+++ SQL数据库/笔记06_约束.sql
@@ -1,26 +1,19 @@
 -- primary key 主键约束
 CREATE TABLE natural_key_example ( 
-	license_id varchar(10) CONSTRAINT license_key PRIMARY KEY,
-	-- license_id varchar(10) PRIMARY KEY
+	license_id varchar(10) PRIMARY KEY,
 	first_name varchar(50),
 	last_name varchar(50) 
 	);
--- 会报错，因为primary key应该唯一
--- ERROR:  duplicate key value violates unique constraint "license_key"
-INSERT INTO natural_key_example(license_id, first_name, last_name)
-VALUES ('T229901', 'Lynn', 'Malero');
-INSERT INTO natural_key_example (license_id, first_name, last_name) 
-VALUES ('T229901', 'Sam', 'Tracy'); 
+
+ALTER TABLE natural_key_example DROP PRIMARY KEY;
+
 
 
 -- -- 如果忘记创建主键
 -- mysql way
--- ALTER TABLE natural_key_example ADD PRIMARY KEY (license_id);
--- -- delete
--- ALTER TABLE natural_key_example DROP PRIMARY KEY;
--- ALTER TABLE natural_key_example MODIFY license_id varchar(10) PRIMARY KEY;
+ALTER TABLE natural_key_example ADD PRIMARY KEY (license_id);
 -- 弃键的约束
-ALTER TABLE natural_key_example DROP CONSTRAINT license_id;
+-- ALTER TABLE natural_key_example DROP CONSTRAINT license_id;
 -- 弃 not null 的约束
 ALTER TABLE natural_key_example ALTER COLUMN first_name DROP NOT NULL;
 
@@ -43,19 +36,17 @@ ALUES(775, '1/23/2017', 'N');
 
 
 -- auto-incrementing surrogate key 自增约束
+-- order_number列自动补全1、2、3
 -- smallserial, serial, and bigserial对应smallint, integer, and bigint
 CREATE TABLE surrogate_key_example (
-	order_number bigserial, 
+	order_number bigserial PRIMARY KEY auto_increment
 	product_name varchar(50), 
-	order_date date,
-	CONSTRAINT order_key PRIMARY KEY (order_number)
-	-- order_number PRIMARY KEY auto_increment
+	order_date date
 	);
 INSERT INTO surrogate_key_example (product_name, order_date) 
 VALUES ('Beachball Polish', '2015-03-17'), 
 ('Wrinkle De-Atomizer', '2017-05-22'),
-('Flux Capacitor', '1985-10-26'); 
--- order_number列自动补全1、2、3
+('Flux Capacitor', '1985-10-26');
 SELECT * FROM surrogate_key_example; 
 
 
@@ -105,11 +96,12 @@ CREATE TABLE check_constraint_example (
 
 -- unique 唯一约束:允许null存在
 -- mysql method:
--- CREATE TABLE user1 (
--- id int,
--- name varchar(20),
--- unique(name)
--- );
+CREATE TABLE user1 (
+id int,
+name varchar(20),
+unique(name)
+);
+					     
 CREATE TABLE unique_constraint_example (
 	contact_id bigserial CONSTRAINT contact_id_key PRIMARY KEY, 
 	first_name varchar(50),
@@ -121,18 +113,20 @@ INSERT INTO unique_constraint_example (first_name, last_name, email)
 VALUES ('Samantha', 'Lee', 'slee@example.org');
 INSERT INTO unique_constraint_example (first_name, last_name, email) 
 VALUES ('Betty', 'Diaz', 'bdiaz@example.org');
-
+					     
+ALTER TABLE unique_constraint_example ADD UNIQUE (name);
+ALTER TABLE unique_constraint_example DROP INDEX name;
+ALTER TABLE not_null_example ADD PRIMARY KEY (student_id);				     
+ALTER TABLE unique_constraint_example MODIFY name varchar(20) UNIQUE; 
+ALTER TABLE natural_key_example MODIFY license_id varchar(10) PRIMARY KEY;					    
 
 -- 非空约束
 CREATE TABLE not_null_example (
-	student_id bigserial,
+	student_id bigserial PRIMARY KEY,
 	first_name varchar(50) NOT NULL,
 	last_name varchar(50) NOT NULL,
-	CONSTRAINT student_id_key PRIMARY KEY (student_id)
 	);
 
-ALTER TABLE not_null_example DROP CONSTRAINT student_id_key;
-ALTER TABLE not_null_example ADD CONSTRAINT student_id_key PRIMARY KEY (student_id);
 ALTER TABLE not_null_example ALTER COLUMN first_name DROP NOT NULL; 
 ALTER TABLE not_null_example ALTER COLUMN first_name SET NOT NULL;
 

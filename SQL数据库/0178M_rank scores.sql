1af884174780f95fe4a364760d56b8cfea9bc162
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-01-30 22:45:21 +0300

    Update 0178M_rank scores.sql

--- SQL数据库/0178M_rank scores.sql
+++ SQL数据库/0178M_rank scores.sql
@@ -2,13 +2,3 @@
 -- 当两个人获得相同分数时，取并列名次，且名次中无断档
 SELECT *, DENSE_RANK() OVER(ORDER BY Score DESC) AS 'Rank'
 FROM Scores;
-
-
--- 拓展
--- 2
--- 找到score表中最高分学生的学号与课程号
-select sno, cno from score where degree=(select max(degree) from score);
-
--- 排序并取出最高分，存在缺陷，万一有两个最高分
--- limit 第一个参数表示位置，第二个参数表示查询多少条
-select sno, cno from score order by degree desc limit 0, 1;

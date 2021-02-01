6a1f316745b795a9595ae9c6ecc48c5b361925dc
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-01 16:41:05 +0300

    自联结

--- SQL数据库/0196E_delete duplicate emails.sql
+++ SQL数据库/0196E_delete duplicate emails.sql
@@ -3,14 +3,12 @@
 DELETE p1 FROM Person p1, Person p2
 WHERE p1.Email = p2.Email AND p1.Id > p2.Id;
 
-
 -- 法二
 DELETE FROM Person
 WHERE Id NOT IN
 (SELECT MinId FROM
  (SELECT Email, MIN(Id) AS MinId FROM Person
   GROUP BY Email) AS tmp);
-
 
 -- 法三
 DELETE FROM Person

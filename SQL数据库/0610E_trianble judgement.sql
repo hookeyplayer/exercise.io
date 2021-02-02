4b507e84da257d5f8f4579f7a635a95b6cc559a7
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-02 18:51:50 +0300

    Case when

--- SQL数据库/0610E_trianble judgement.sql
+++ SQL数据库/0610E_trianble judgement.sql
@@ -4,8 +4,7 @@ select *,
     FROM triangle;
 
 -- case when
-SELECT *,
-CASE WHEN
-(x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
-ELSE 'No' END AS triangle
+SELECT *, CASE WHEN (x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
+               ELSE 'No' 
+               END AS triangle
 FROM triangle

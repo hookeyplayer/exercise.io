33a91ce7403005a51f166e830ffb372c3d2e7c05
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-02 19:09:26 +0300

    自联结

--- SQL数据库/0181E_employees earning more than their managers.sql
+++ SQL数据库/0181E_employees earning more than their managers.sql
@@ -10,12 +10,3 @@ SELECT a.Name AS 'Employee'
 FROM Employee AS a, Employee AS b
 WHERE a.ManagerId = b.Id
 AND a.Salary > b.Salary;
-
--- 法三
-Select Name as Employee from 
-(select p.*,q.sALARY AS MANAGERsalary
-from Employee p 
-left join 
-Employee q 
-ON p.MANAGERid = q.ID) t 
-where sALARY > MANAGERsalary

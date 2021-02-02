8a5d472540d121eddaa46728ef04aa3d2daeb539
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-02 18:59:13 +0300

    创建函数，窗口函数

--- SQL数据库/0177M_nth highest salary.sql
+++ SQL数据库/0177M_nth highest salary.sql
@@ -11,6 +11,18 @@ BEGIN
      ) AS getNthHighestSalary);
 END;
 
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (     
+  SELECT  IF(count<N,NULL,min) 
+  FROM
+    (SELECT MIN(Salary) AS min, COUNT(1) AS count
+    FROM
+      (SELECT DISTINCT Salary
+      FROM Employee ORDER BY Salary DESC LIMIT N) AS a
+    ) as b
+  );
+END
 
 -- 2窗口函数
 SELECT MAX(CASE WHEN rnk = 2 THEN Salary ELSE null END) AS SecondHighestSalary 

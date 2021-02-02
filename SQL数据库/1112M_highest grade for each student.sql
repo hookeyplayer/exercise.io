1fdd1e5035d60222cc8e440ff11264547f7768b2
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-02 09:32:34 +0300

    row_number()

--- SQL数据库/1112M_highest grade for each student.sql
+++ SQL数据库/1112M_highest grade for each student.sql
@@ -3,8 +3,9 @@
 -- 如果最高成绩相同则，输出课程编号最小的课程
 -- 查询结果按照 student_id 升序排列
 SELECT student_id, course_id, grade FROM (
-SELECT *, ROW_NUMBER() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id ASC) AS grade_rnk
-FROM Enrollments
+	SELECT *,
+	ROW_NUMBER() OVER (PARTITION BY student_id ORDER BY grade DESC, course_id ASC) AS grade_rnk
+	FROM Enrollments
 ) AS tmp
 WHERE grade_rnk = 1
 ORDER BY student_id;

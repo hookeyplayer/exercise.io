16894e3955e7357841a4e3a353ceffd578d83439
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-02-20 18:53:01 +0300

    Add files via upload

--- simu_portfolio/时间序列_协整.R
+++ simu_portfolio/时间序列_协整.R
@@ -32,3 +32,44 @@ summary(ur.df(coin$resid)) # Reject
 # e.g., same industry companies may grow similarly;
 # spot and forward price of financial product are bound together by no-arbitrage principle;
 # FX rates of interlinked countries tend to move together.
+
+# Two variable: Engle-Granger
+# Multivariate: Johansen procedure
+
+library(quantmod)
+getSymbols('DTB3', src = 'FRED')
+getSymbols('DTB6', src = 'FRED')
+DTB3.sub = DTB3['1999-03-18/2021-02-01']
+DTB6.sub = DTB6['1999-03-18/2021-02-01']
+plot(DTB3.sub)
+lines(DTB6.sub, col = 'pink')
+
+# ADF test ----------------------------------------------------------------
+a = as.numeric(na.omit(DTB3.sub))
+b = as.numeric(na.omit(DTB6.sub))
+y = cbind(a, b)
+cointregr <- lm(a ~ b)
+r = cointregr$residuals
+library(urca)
+summary(ur.df(r,type = 'none'))
+
+# Phillips and Ouliaris test ----------------------------------------------
+# NULL: Two series are not cointegrated. 
+library(tseries)
+pocointregr <- po.test(y, demean = T, lshort = T)
+pocointregr$parameter
+pocointregr$p.value
+
+# Johansen Procedure test -------------------------------------------------
+# K is lag order
+johansentest <- ca.jo(y, type = c('trace'), ecdet = c('none'), K = 2)
+summary(johansentest)
+# The test statistic for r = 0 is 113.71 > critical, indicating rejection of NULL;
+# for r  ≤ 1, cannot reject NULL.
+# Thus, conclude that one cointegrating relationship exists.
+yJoregr = cajorls(johansentest, r = 1)
+yJoregr
+# The coefficient of error-correction term is negative:
+# short-term deviation from long-term equilibrium would push to zero-equilibrium
+
+

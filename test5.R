d8233dfbf411fddcae40ed4949329ca2822b638d
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-03-27 15:02:36 +0300

    列联表，factor

--- test5.R
+++ test5.R
@@ -2,6 +2,7 @@ library(xlsx)
 msci2017 <- read.xlsx('msci2017.xlsx', 1,head = TRUE)
 head(msci2017)
 str(msci2017)
+
 # 按国家统计 2017
 country <- factor(msci2017$Country,
                   levels = c('Brazil', 'Chile', 'China', 'Colombia',
@@ -13,8 +14,26 @@ country <- factor(msci2017$Country,
                   )
 head(country)
 table(country)
+
 # 按版块统计
 msci2019 <- read.xlsx('msci2019.xlsx', 1,head = TRUE)
+sector <- factor(msci2017$Sector,
+                 levels = c('Communication Services', 'Consumer Discretionary',
+                            'Consumer Staples', 'Energy', 'Financials',
+                            'Health Care', 'Industrials', 'Materials',
+                            'Information Technology', 'Real Estate', 'Utilities')
+                 )
+
+a <- table(country, sector)
+a
+margin.table(a, 1) # 行
+margin.table(a, 2) # 列
+
+prop.table(a, 2)
+
+# 导出
+write.xlsx(a, "/Users/xiaofan/论文数据/contingency_table.xlsx",
+           sheetName = "2017", append = TRUE)
 
 # 按国家统计 2017
 country2 <- factor(msci2019$Country,
@@ -27,3 +46,17 @@ country2 <- factor(msci2019$Country,
                              'Saudi Arabia')
                   )
 table(country2)
+sector2 <- factor(msci2019$Sector,
+                 levels = c('Communication Services', 'Consumer Discretionary',
+                            'Consumer Staples', 'Energy', 'Financials',
+                            'Health Care', 'Industrials', 'Materials',
+                            'Information Technology', 'Real Estate', 'Utilities')
+)
+b <- table(country2, sector2)
+b
+write.xlsx(b, "/Users/xiaofan/论文数据/contingency_table.xlsx",
+           sheetName = "2019", append = TRUE)
+
+
+# gl,seq,rep
+# rt, rnorm

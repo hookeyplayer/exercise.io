849954a481a577a2665eef966677bb51cfe73a0b
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-06-06 13:47:20 +0300

    Delete 1082E_sales analysis Ⅰ.sql

--- SQL数据库/1082E_sales analysis Ⅰ.sql
+++
@@ -1,9 +0,0 @@
--- 产品表和销售表
--- 查询全部总销售价格最高的销售员，返回其 seller_id
-WITH tmp AS (
-SELECT seller_id, RANK() OVER(ORDER BY SUM(price) DESC) AS rnk FROM Sales
-GROUP BY seller_id
-)
-
-SELECT seller_id FROM tmp
-WHERE rnk = 1;

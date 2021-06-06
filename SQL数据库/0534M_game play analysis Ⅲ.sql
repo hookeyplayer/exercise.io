0b316c24341f96f5dd8bb3269dc1f3a15f642eb6
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-06-06 21:26:22 +0300

    Delete 0534M_game play analysis Ⅲ.sql

--- SQL数据库/0534M_game play analysis Ⅲ.sql
+++
@@ -1,8 +0,0 @@
--- 按照日期，查询每个用户累积玩的游戏数
-
--- 关联子查询
-SELECT a.player_id, a.event_date,
- (SELECT SUM(games_played) FROM Activity AS b
-  WHERE a.player_id = b.player_id
-  AND a.event_date >= b.event_date) AS games_played_so_far
-FROM Activity AS a

fe6c0d341a912b1636a41ae14ee22ba94e0a6075
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-06-13 20:05:06 +0300

    Delete 0608M_tree node.sql

--- SQL数据库/0608M_tree node.sql
+++
@@ -1,7 +0,0 @@
--- 列出每一个节点及其所属类型
-SELECT Id,
-CASE WHEN p_id IS NULL THEN 'Root'
-     WHEN id NOT IN (SELECT DISTINCT p_id FROM tree
-                     WHERE p_id IS NOT NULL) THEN 'Leaf'
-     ELSE 'Inner' END AS Type
-FROM tree;

e130799d5dd388e14cb055b7a58ff4bca4fd9a2b
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-06-13 13:45:38 +0300

    Delete 1543E_fix product name format.sql

--- SQL数据库/1543E_fix product name format.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-	LOWER(TRIM(product_name)) AS product_name, 
-	LEFT(sale_date, 7) AS sale_date,
-	COUNT(*) AS total
-FROM Sales
-GROUP BY LOWER(TRIM(product_name)), LEFT(sale_date, 7)
-ORDER BY product_name, sale_date;

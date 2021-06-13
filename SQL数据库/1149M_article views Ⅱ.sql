07e1518dfeda78d7d1134f345d8cc3f102605769
hookeyplayer <60797525+hookeyplayer@users.noreply.github.com>
2021-06-13 20:25:06 +0300

    Delete 1149M_article views Ⅱ.sql

--- SQL数据库/1149M_article views Ⅱ.sql
+++
@@ -1,8 +0,0 @@
--- 查询在同一日期查看过不止一篇文章的所有用户，并按ID升序排列
-SELECT DISTINCT viewer_id id
-FROM (
-	SELECT viewer_id, view_date FROM Views
-	GROUP BY viewer_id, view_date
-	HAVING COUNT(DISTINCT article_id) > 1
-) tmp
-ORDER BY id;
